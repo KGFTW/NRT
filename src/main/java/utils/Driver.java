8ea64186adc34c23aacfc0a0e8c497e3ed8e3e0d
Tropee, Sylvain <slythenegger@hotmail.fr>
2018-07-02 15:22:32 +0200

    with no sans dbox and // disable dev

--- src/main/java/utils/Driver.java
+++ src/main/java/utils/Driver.java
@@ -140,8 +140,8 @@ public abstract class Driver {
 			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--headless");
-			//options.addArguments("--no-sandbox");
-       		options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--no-sandbox");
+       		//options.addArguments("--disable-dev-shm-usage");
 			options.addArguments("--disable-gpu");
 			options.addArguments("--window-size=1920,1080");
 			//options.setBinary("resources/GoogleChromePortable/GoogleChromePortable.exe");
