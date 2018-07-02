3e14f222a990a1271c1bf8a1c4b7e7f6214eb1d0
Tropee, Sylvain <slythenegger@hotmail.fr>
2018-07-02 15:20:34 +0200

    with   // no  sand boxx

--- src/main/java/utils/Driver.java
+++ src/main/java/utils/Driver.java
@@ -139,9 +139,8 @@ public abstract class Driver {
 			System.setProperty("webdriver.chrome.driver", getChromePath());
 			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 			ChromeOptions options = new ChromeOptions();
-			//options.setExperimentalOption("useAutomationExtension", false);
 			options.addArguments("--headless");
-			options.addArguments("--no-sandbox");
+			//options.addArguments("--no-sandbox");
        		options.addArguments("--disable-dev-shm-usage");
 			options.addArguments("--disable-gpu");
 			options.addArguments("--window-size=1920,1080");
